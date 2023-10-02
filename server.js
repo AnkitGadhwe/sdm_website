c2d855fcb1460c2ee0c2f02358b7cf8983386ab6
Ankit Gadhwe <ankitgadhwe3830@gmail.com>
2023-10-02 21:21:47 +0530

    Change Path of server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var app=express();
-app.use(express.static('public'))
+app.use(express.static('index.html'))
 app.get("/",(req, res)=>{res.send("Welcome to  IET");});
 app.get("/product",(req, res)=>{var p={ "id":45, "title":"Gerbera","description":"Wedding Flower", "unitprice":14, "quatity":7000 };
                                        res.send(p);
